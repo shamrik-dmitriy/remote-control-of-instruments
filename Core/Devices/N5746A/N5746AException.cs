e100f225f918025104312f7a3aed1dc3a2e31c3c
Dmitriy S <dmitriy@ckba.com>
2019-09-17 17:30:55 +0600

    Create structure in Core
    Creating structure Core project:
     - added folder for devices;
     - added folder for exchange;
     - added blank classes for devices;
     - added blank class for exchange;
     - added application model class.

---
+++ Core/Devices/N5746A/N5746AException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Devices.N5746A
+{
+    class N5746AException
+    {
+    }
+}
