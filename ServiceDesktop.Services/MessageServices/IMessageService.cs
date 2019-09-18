375f074f11fcdbec44db416ed129d81c862f5ecd
Dmitriy S <dmitriy@ckba.com>
2019-09-18 14:23:40 +0600

    Create project for services. Create message services for output message

---
+++ ServiceDesktop.Services/MessageServices/IMessageService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceDesktop.Services.MessageServices
+{
+    public interface IMessageService
+    {
+        void ShowMessage(string message);
+        void ShowMessage(string message, string cation);
+        void ShowExclamation(string message);
+        void ShowExclamation(string message, string caption);
+        void ShowError(string message);
+        void ShowError(string message, string caption);
+        void ShowWarning(string message);
+        void ShowWarning(string message, string caption);
+    }
+}
---
+++ ServiceDesktop.Services/MessageServices/MessageService.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows.Forms;
+
+namespace ServiceDesktop.Services.MessageServices
+{
+    public class MessageService
+    {
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void ShowMessage(string message, string caption)
+        {
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void ShowExclamation(string message)
+        {
+            MessageBox.Show(message, "Exclamation", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        public void ShowExclamation(string message, string caption)
+        {
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        public void ShowError(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public void ShowError(string message, string caption)
+        {
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public void ShowWarning(string message, string caption)
+        {
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        public void ShowWarning(string message)
+        {
+            MessageBox.Show(message, "Exclamation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
