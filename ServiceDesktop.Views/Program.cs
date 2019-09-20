1c4a5945c897cc121c21df387ea23a24fe13c8da
Dmitriy S <dmitriy@ckba.com>
2019-09-20 10:27:41 +0600

    Update Program class
    Updating Programm class:
     - devided regions;
     - added method for parse version software;
     - added properties and variables for using version.

--- ServiceDesktop.Views/Program.cs
+++ ServiceDesktop.Views/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Core;
@@ -13,6 +14,32 @@ namespace ServiceDesktop.Views
 {
     static class Program
     {
+        #region Private Properties
+
+        /// <summary>
+        ///     Flag for develop or master version software
+        /// </summary>
+        private static bool DevelopVersion { get; } = true;
+
+        #endregion
+
+        #region Private Member Variables
+
+        /// <summary>
+        ///     Name of Software
+        /// </summary>
+        private const string Name = "Remote Control Power Supply and Signal Generator";
+
+        #endregion
+
+        #region  Public properties
+
+        public static string Title => $"{Name} {GetCurrentVersion}";
+
+        #endregion
+
+        #region Constructor
+
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
@@ -28,5 +55,28 @@ namespace ServiceDesktop.Views
                 new ServiceDesktopPresenter(new MainForm(), new ServiceDesktopModel(), new MessageService());
             presenter.Run();
         }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        ///     Get current software version
+        /// </summary>
+        private static string GetCurrentVersion
+        {
+            get
+            {
+                var prepareVersion = new StringBuilder();
+                var versionApp = Version.Parse(Application.ProductVersion);
+                prepareVersion.Append(versionApp.Major).Append(".").Append(versionApp.Minor);
+                prepareVersion.Append(versionApp.Build > 0 ? "." + versionApp.Build : "");
+                prepareVersion.Append(versionApp.Revision > 0 ? "." + versionApp.Build : "");
+                prepareVersion.Append(DevelopVersion ? " Develop version" : "");
+                return prepareVersion.ToString();
+            }
+        }
+
+        #endregion
     }
 }
