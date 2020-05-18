0fb12d1e62b22005f6f97c8f54a9391d4db0b427
shamrik-dmitriy <shamrik.dmitriy@gmail.com>
2020-05-18 23:28:46 +0600

    Creating settings form in Views project

---
+++ ServiceDesktop.Views/SettingsView.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace RCLD.Views
+{
+    partial class SettingsView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxDevices = new System.Windows.Forms.ComboBox();
+            this.textBoxIpAddress = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxDevices
+            // 
+            this.comboBoxDevices.FormattingEnabled = true;
+            this.comboBoxDevices.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxDevices.Name = "comboBoxDevices";
+            this.comboBoxDevices.Size = new System.Drawing.Size(234, 21);
+            this.comboBoxDevices.TabIndex = 0;
+            // 
+            // textBoxIpAddress
+            // 
+            this.textBoxIpAddress.Location = new System.Drawing.Point(12, 39);
+            this.textBoxIpAddress.Name = "textBoxIpAddress";
+            this.textBoxIpAddress.Size = new System.Drawing.Size(156, 20);
+            this.textBoxIpAddress.TabIndex = 1;
+            this.textBoxIpAddress.Text = "IP";
+            this.textBoxIpAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(174, 39);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(72, 20);
+            this.textBoxPort.TabIndex = 2;
+            this.textBoxPort.Text = "Порт";
+            this.textBoxPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(12, 65);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(234, 23);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // SettingsView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(257, 99);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIpAddress);
+            this.Controls.Add(this.comboBoxDevices);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SettingsView";
+            this.Text = "Настройки";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxDevices;
+        private System.Windows.Forms.TextBox textBoxIpAddress;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
