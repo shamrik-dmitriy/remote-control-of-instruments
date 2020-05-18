42e98ed51a7fde7a33b14edfc0cd1d38988ffd7f
shamrik-dmitriy <shamrik.dmitriy@gmail.com>
2020-05-18 21:59:01 +0600

    Added Attributes Services
    Added attributes services:
     - create attribute for enum description.

---
+++ Services/AttributesServices/EnumDescriptionAttributes/EnumDescriptionAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Services.AttributesServices.EnumDescriptionAttributes
+{
+    /// <summary>
+    ///     Класс атрибутов предназначенный для перечислений
+    ///     Позволяет задавать описание к элементу перечисления и получать его
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Enum | AttributeTargets.Field)]
+    public sealed class EnumDescriptionAttribute : Attribute
+    {
+        #region Public properties
+
+        /// <summary>
+        ///     Get description for enumerated type
+        /// </summary>
+        public string Description { get; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        ///     Initialize new instance
+        /// </summary>
+        /// <param name="description">Description attribute value</param>
+        public EnumDescriptionAttribute(string description)
+        {
+            Description = description;
+        }
+
+        #endregion
+    }
+}
---
+++ Services/AttributesServices/EnumDescriptionAttributes/EnumDescriptionAttributeHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+
+namespace Services.AttributesServices.EnumDescriptionAttributes
+{
+    /// <summary>
+    ///     Класс предоставляет вспомогательные методы для работы с атрибутами 
+    /// </summary>
+    public static class EnumDescriptionAttributeHelper
+    {
+        #region Public Methods
+
+        /// <summary>
+        ///     Возвращает текстовое описание значения перечисления <see cref="valueOfEnum"/>
+        /// </summary>
+        /// <param name="valueOfEnum">Значение перечисления</param>
+        /// <returns>Строковое представление значения перечисления</returns>
+        public static string GetDescription(this Enum valueOfEnum)
+        {
+            if (valueOfEnum == null) throw new ArgumentNullException("valueOfEnum");
+
+            var description = valueOfEnum.ToString();
+            var fieldInfo = valueOfEnum.GetType().GetField(description);
+
+            var attributes =
+                (EnumDescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(EnumDescriptionAttribute), false);
+
+            if (attributes.Length > 0) description = attributes[0].Description;
+
+            return description;
+        }
+
+        /// <summary>
+        ///     Возвращает универсальную коллекцию объектов типа перечисления
+        /// </summary>
+        /// <param name="type">Тип</param>
+        /// <returns>Универсальная коллекция объектов типа перечисления</returns>
+        public static IList ToList(this Type type)
+        {
+            if (type == null) throw new ArgumentNullException("type");
+
+            var list = new ArrayList();
+
+            foreach (Enum value in Enum.GetValues(type)) list.Add(GetDescription(value));
+
+            return list;
+        }
+
+        #endregion
+    }
+}
