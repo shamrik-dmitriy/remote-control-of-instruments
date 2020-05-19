6846281ab6259035e15a7bcbe6afb49da79e6516
shamrik-dmitriy <shamrik.dmitriy@gmail.com>
2020-05-19 22:58:22 +0600

    Update Services project
    Update Services project part of exchange:
     - added Exchange Service submodule:
      - added exception class;
      - added shared interface;
      - added Socket Exchange submodule.

---
+++ Services/ExchangeServices/ExchangeServiceException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ExchangeServices
+{
+    /// <summary>
+    ///     Специфический класс исключений, предназначенный для исключений при работе со службой обмена
+    /// </summary>
+    [Serializable]
+    public sealed class ExchangeServiceException : ApplicationException
+    {
+        public ExchangeServiceException()
+        {
+        }
+
+        public ExchangeServiceException(string message) : base(message)
+        {
+        }
+
+        public ExchangeServiceException(string message, SystemException innerException) : base(message,
+            innerException)
+        {
+        }
+
+        protected ExchangeServiceException(SerializationInfo serializationInfo, StreamingContext streamingContext) :
+            base(
+                serializationInfo, streamingContext)
+        {
+        }
+    }
+}
